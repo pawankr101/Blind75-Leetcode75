4d4cbc8ff3cff6ecaed92f2c5a292e93dee4a4b8
Akshay Kumar <pawan.akshaykr@gmail.com>
2023-09-19 21:03:31 +0530

    Runtime 47 ms / Memory 41.87 MB

--- Two_Pointers/011-Is_Subsequence/solution.js
+++ Two_Pointers/011-Is_Subsequence/solution.js
@@ -4,5 +4,16 @@
  * @return {boolean}
  */
 var isSubsequence = function(s, t) {
-    
+    const sl = s.length, tl = t.length;
+    if(sl>tl) return false;
+    let i = 0, j = 0;
+    while(i<sl && j<tl) {
+        if(s[i] === t[j]) {
+            i++;
+            j++;
+        } else j++;
+    }
+    return (i===sl);
 };
+
+console.log(isSubsequence('abc', 'ahbgdc'))
