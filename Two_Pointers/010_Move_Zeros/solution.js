c8273648f1b2e63511a36f5d34b484a2d44d4e1f
Akshay Kumar <pawan.akshaykr@gmail.com>
2023-09-19 12:06:46 +0530

    Runtime 75 ms / Memory 46.10 MB

--- Two_Pointers/010_Move_Zeros/solution.js
+++ Two_Pointers/010_Move_Zeros/solution.js
@@ -3,5 +3,20 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    
+    const len = nums.length;
+    let start = 0, end = 1;
+    while(end<len) {
+        if(!nums[end]) end++;
+        else if(nums[start]) {
+            start++;
+            if(start===end) end++;
+        }
+        else {
+            nums[start++] = nums[end];
+            nums[end++] = 0;
+        }
+    }
+    return nums;
 };
+
+console.log(moveZeroes([0]))
