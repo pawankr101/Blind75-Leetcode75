9a6913a672184f8cc193c40da19ef9fde0c61658
Akshay Kumar <pawan.akshaykr@gmail.com>
2023-09-20 17:03:35 +0530

    Runtime 59 ms / Memory 49.87 MB

--- Two_Pointers/012-Container_With_Most_Water/solution.js
+++ Two_Pointers/012-Container_With_Most_Water/solution.js
@@ -3,5 +3,15 @@
  * @return {number}
  */
 var maxArea = function(heights) {
-    
+    let maxArea=0, i=0, j=heights.length-1, area, smallSideHeight;
+    while(i<j) {
+        smallSideHeight = heights[i]<heights[j] ? heights[i]: heights[j]
+        area=(j-i)*smallSideHeight;
+        if(area>maxArea) {
+            maxArea=area;
+        }
+        i++;
+        j--;
+    }
+    return maxArea;
 };
