ac90296c06ca1913bf8cd70b8429c5c0d7811412
Akshay Kumar <pawan.akshaykr@gmail.com>
2024-05-16 10:51:18 +0530

    Runtime 74 ms / Memory 58.48 MB

--- Two_Pointers/013-Max_Number_of_K-Sum_Pairs/solution.js
+++ Two_Pointers/013-Max_Number_of_K-Sum_Pairs/solution.js
@@ -1,8 +1,43 @@
+// /**
+//  * brute force solution
+//  * @param {number[]} nums
+//  * @param {number} k
+//  * @return {number}
+//  */
+// var maxOperations = function(nums, k) {
+//     let oc = 0;
+//     for(let i = 0, l = nums.length, j; i < l-1; i++) {
+//         if(nums[i]!=-1) {
+//             for (j = i+1; j < l; j++) {
+//                 if(nums[j]!=-1 && (nums[i] + nums[j] === k)) {
+//                     nums[i] = -1
+//                     nums[j] = -1
+//                     oc++;
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+//     return oc;
+// };
+
 /**
  * @param {number[]} nums
  * @param {number} k
  * @return {number}
  */
 var maxOperations = function(nums, k) {
-    
+    /** @type {Map<number, number>} */
+    const map = new Map(), len = nums.length;
+    let oc = 0, i = 0;
+    while(i<len) {
+        let n = nums[i++]
+        if(n<k) {
+            if(map.get(n)) {
+                oc++;
+                map.set(n, map.get(n) - 1);
+            } else map.set(k-n, (map.get(k-n) || 0) + 1);
+        }
+    }
+    return oc;
 };
